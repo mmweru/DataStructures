a276851e420576b4049fb2edc5ff1fbe59599dac
Maryann Mweru <mwerumaryann@gmail.com>
2023-09-18 17:49:08 +0300

    Adjanceny list code

--- binary.py
+++ binary.py
@@ -20,6 +20,51 @@ class Node:
                     self.right.insert(data)
 
 
+# Inorder traversal
+def inOrderPrint(r):
+    if r is None:
+        return
+    else:
+        inOrderPrint(r.left)
+        print(r.data, end=' ')
+        inOrderPrint(r.right)
+
+
+# Preorder traversal
+def preOrderPrint(r):
+    if r is None:
+        return
+    else:
+        print(r.data, end=' ')
+        preOrderPrint(r.left)
+        preOrderPrint(r.right)
+
+
+# PostOrderPrint
+def postOrderPrint(r):
+    if r is None:
+        return
+    else:
+        postOrderPrint(r.left)
+        postOrderPrint(r.right)
+        print(r.data, end=' ')
+
+
+# Adjacency List
+def makeList(r):
+    if r is None:
+        return
+    else:
+        d[r.data] = []
+        makeList(r.left)
+        if r.left:
+            d[r.data].append(r.left.data)
+        if r.right:
+            d[r.data].append(r.right.data)
+        makeList(r.right)
+    return d
+
+
 if __name__ == '__main__':
     root = Node('g')
     root.insert('c')
@@ -31,3 +76,7 @@ if __name__ == '__main__':
     root.insert('i')
     root.insert('h')
     root.insert('k')
+
+    d = {}
+    aList = makeList(root)
+    print(aList)
